a5ca6d5cd28e3dbba23d203e160a49ec97c4be16
jorge-alonso <jorge.a.alonso91@gmail.com>
2021-10-16 00:53:43 -0300

    Utilizando vuex, se agrega contador de numeros pares e impares en el componente de helloworld

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,27 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const state = {
+  count: 0
+}
+const mutations = {
+  increment (state) {
+    state.count++
+  },
+  decrement (state) {
+    state.count--
+  }
+}
+const actions = {
+  increment: ({ commit }) => commit('increment'),
+  decrement: ({ commit }) => commit('decrement')
+}
+const getters = {
+  evenOrOdd: state => state.count % 2 === 0 ? 'par' : 'impar'
+}
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state,
+  getters,
+  actions,
+  mutations,
+})
